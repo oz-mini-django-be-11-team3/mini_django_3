f5fc72f252616c99ba30e08f6c89e63048b71d19
박준혁 <jun201908@naver.com>
2025-07-18 15:50:52 +0900

    ✨feat : users model 정의
    1) users/models.py에 model 정의
    2) settings.py / INSTALLED_APPS에 UsersConfig 추가
    3) settings.py에 맨밑줄에 AUTH_USER_MODEL 추가

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "apps.common.apps.CommonConfig",
+    "apps.users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.CustomUserModel"
