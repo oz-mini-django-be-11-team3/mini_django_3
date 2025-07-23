173e0a0814e1a89b39652ddb56c2cf07ce9a6fee
loveyoun <lovekite0406@naver.com>
2025-07-23 18:14:38 +0900

    codedeploy

--- app/config/settings.py
+++ app/config/settings.py
@@ -163,6 +163,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# 만약 개발 모드라면 보통 이렇게 지정
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
