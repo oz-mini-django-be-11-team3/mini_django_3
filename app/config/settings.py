b09d5909fd79734e69c1cd2c34ff4e9cc71bd3e1
loveyoun <lovekite0406@naver.com>
2025-07-23 16:09:12 +0900

    codedeploy

--- app/config/settings.py
+++ app/config/settings.py
@@ -114,11 +114,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "qwert"),
         "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "PORT": os.getenv("DB_PORT"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "NAME": os.getenv("DB_NAME","postgres"),
     }
     # 'default': {
     #     # 'ENGINE': 'django.db.backends.sqlite3',
---
+++ app/start.sh
@@ -0,0 +1,4 @@
+cd /home/ec2-user/oz
+docker-compose down
+docker-compose build
+docker-compose up -d
